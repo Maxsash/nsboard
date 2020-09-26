a7539d620d52a089d26d530724542ac78acfa33d
Maxsash <the.revived.1@gmail.com>
2020-09-26 21:19:23 +0530

    Complete first game

--- examples/example1.py
+++ examples/example1.py
@@ -1,25 +1,38 @@
-#import random
+import getpass
 
 import netsquid as ns
-#from netsquid.nodes import Node
-#from netsquid.components import QuantumChannel, Channel
-#from netsquid.nodes import DirectConnection
 from netsquid_boardgames.protocols import TaskMasterProtocol, PlayerProtocol
 from netsquid_boardgames.network import board_network
 
 if __name__ == "__main__":
-	board_size = 10
+	
+	board_size = int(input("Enter the size of the Main Memory: "))
+	#Building the network
 	network = board_network(board_size)
 
+	#Get the ports from connections
 	classical_ports_tm0 = network.get_connected_ports('TaskMasterNode', 'PlayerZeroNode', 'TMP0')
 	classical_ports_tm1 = network.get_connected_ports('TaskMasterNode', 'PlayerOneNode', 'TMP1')
 	quantum_ports_tm0 = network.get_connected_ports('TaskMasterNode', 'PlayerZeroNode', 'QTMP0')
 	quantum_ports_tm1 = network.get_connected_ports('TaskMasterNode', 'PlayerOneNode', 'QTMP1')
 
+	#Initialize Nodes with Protocols
 	protocols = TaskMasterProtocol(network.nodes['TaskMasterNode'], board_size, classical_ports_tm0[0], quantum_ports_tm0[0], classical_ports_tm1[0], quantum_ports_tm1[0]).start()
 
-	playerZero = PlayerProtocol(0, quantum_ports_tm0[1], classical_ports_tm0[1], network.nodes['PlayerZeroNode']).start()
-	playerOne = PlayerProtocol(1, quantum_ports_tm1[1], classical_ports_tm1[1], network.nodes['PlayerOneNode']).start()
-	#print(network.nodes['TaskMasterNode'].subcomponents['BufferMemoryZero'])
+	playerZero = PlayerProtocol(0, board_size, quantum_ports_tm0[1], classical_ports_tm0[1], network.nodes['PlayerZeroNode']).start()
+	playerOne = PlayerProtocol(1, board_size, quantum_ports_tm1[1], classical_ports_tm1[1], network.nodes['PlayerOneNode']).start()
+
 	ns.sim_run()
 
+	print("Game finished. All moves played. Take a look at the measurements of the board.")
+
+	score = 0
+	for i in range(board_size):
+		a = network.nodes['TaskMasterNode'].qmemory.measure(i)
+		score += a[0][0] * a[1][0]
+		print(a)
+	print("The score is ", score)
+	if score < board_size/2:
+		print("Player Zero Wins")
+	else:
+		print("Player One Wins")
--- netsquid_boardgames/protocols.py
+++ netsquid_boardgames/protocols.py
@@ -2,16 +2,19 @@ import logging, sys
 #logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 import random
+import getpass
 
 import netsquid as ns
 from netsquid.protocols import NodeProtocol
 from netsquid.components import QuantumMemory
 import netsquid.components.instructions as instr
+import netsquid.util.simtools
 
 class TaskMasterProtocol(NodeProtocol):
 	def __init__(self, node, memory_size, cport_name0, qport_name0, cport_name1, qport_name1):
 		super().__init__(node)
 		self.node = node
+
 		self._qport_name0 = qport_name0
 		self._cport_name0 = cport_name0
 		self._qport_name1 = qport_name1
@@ -19,19 +22,22 @@ class TaskMasterProtocol(NodeProtocol):
 
 		self.size = memory_size
 		self.memory = self.node.qmemory
-
 		self.memory.reset()
 		qubit = ns.qubits.create_qubits(self.size)
 
 		self.memory.put(qubit)
 		logging.debug('tm: put qubits in memory')
 
-		self.index = random.randint(0, self.size)
+		self.index = random.randint(0, self.size-1)
+		
+		self.counter = 0
 
 		logging.debug('tm: init complete')
 
 	def run(self):
-		while True:
+		while self.counter < (self.size/2):
+			
+			self.counter += 1
 
 			qport0 = self.node.ports[self._qport_name0]
 			cport0 = self.node.ports[self._cport_name0]
@@ -48,12 +54,12 @@ class TaskMasterProtocol(NodeProtocol):
 			self.memory.pop(positions=self.index)
 			logging.debug('tm: pop qubit index')
 
-			print("Beforep0: ")
+			logging.debug("Beforep0: ")
 			for i in range(self.size):
 				try:
-					print(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
+					logging.debug(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
 				except:
-					print('None')
+					logging.debug('None')
 
 
 			#Wait for return qubit
@@ -61,12 +67,12 @@ class TaskMasterProtocol(NodeProtocol):
 			yield self.await_port_input(qport0)
 			logging.debug('tm: recieved return qubit from p0')
 
-			print("Afterp0: ")
+			logging.debug("Afterp0: ")
 			for i in range(self.size):
 				try:
-					print(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
+					logging.debug(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
 				except:
-					print('None')
+					logging.debug('None')
 
 
 			#Send acknowledgement
@@ -89,12 +95,12 @@ class TaskMasterProtocol(NodeProtocol):
 			self.memory.pop(positions=self.index)
 			logging.debug('tm: pop qubit index')
 
-			print("Beforep1: ")
+			logging.debug("Beforep1: ")
 			for i in range(self.size):
 				try:
-					print(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
+					logging.debug(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
 				except:
-					print('None')
+					logging.debug('None')
 
 
 			#Wait for return qubit
@@ -102,12 +108,12 @@ class TaskMasterProtocol(NodeProtocol):
 			yield self.await_port_input(qport1)
 			logging.debug('tm: recieved return qubit from p1')
 
-			print("Afterp1: ")
+			logging.debug("Afterp1: ")
 			for i in range(self.size):
 				try:
-					print(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
+					logging.debug(instr.INSTR_MEASURE_X(self.memory, positions=[i]))
 				except:
-					print('None')
+					logging.debug('None')
 
 
 			#Send acknowledgement
@@ -120,15 +126,16 @@ class TaskMasterProtocol(NodeProtocol):
 			logging.debug('tm: recieved the next index from p1')
 
 			self.index = cport1.rx_input().items[0]
-
+		
 
 class PlayerProtocol(NodeProtocol):
-	def __init__(self, player_index, qport_name, cport_name, node=None, name=None):
+	def __init__(self, player_index, memory_size, qport_name, cport_name, node=None, name=None):
 		super().__init__(node=node, name=name)
 		self._qport_name = qport_name
 		self._cport_name = cport_name
 		self.memory = self.node.qmemory
 		self.tag = "p" + str(player_index)
+		self.size = memory_size
 		logging.debug(self.tag + ': init complete')
 
 	def run(self):
@@ -138,14 +145,14 @@ class PlayerProtocol(NodeProtocol):
 
 			logging.debug(self.tag + ': waiting for input')
 			yield self.await_port_input(qport)
-			print(self.tag, ' received', self.memory.peek([0]))
+			logging.debug(self.tag, ' received', self.memory.peek([0]))
 
 			#Operate on qubit here--------------------------------------------------------------------------------
 			operation = None
 			ops = ['X','Y','Z','H','K','S','T','I']
 			#Ask the player what they want to do with the qubit
 			while operation not in ops:
-				operation = input(self.tag + "Enter the operation you want to perform on qubit: ")
+				operation = getpass.getpass(self.tag + " Enter the operation you want to perform on qubit: ").upper()
 				if operation not in ops:
 					print("Invalid operation. Choose from ", ops)
 			
@@ -154,7 +161,7 @@ class PlayerProtocol(NodeProtocol):
 			str1 = "INSTR_" + operation
 			#implement instr.INSTR_x(self.memory, positions=[0]) || x can be any operation on qubit 
 			getattr(instr, str1)(self.memory, positions=[0])
-			print(self.memory.measure(0))
+			logging.debug(self.memory.measure(0))
 			#----------------------------------------------------------------------------------------------------
 			
 			self.memory.pop(positions=0)
@@ -166,6 +173,12 @@ class PlayerProtocol(NodeProtocol):
 			logging.debug(self.tag + ': recieved the ack signal')	
 
 			#Send the next index
-			self.index = input("Enter the next index: ")		
+			next_index = None
+			while next_index not in range(0, self.size):
+				next_index = int(getpass.getpass("Enter the next index: "))
+				if next_index not in range(0, self.size):
+					print("Invalid index. Choose between 0 and ", self.size-1)
+			self.index = next_index
+
 			cport.tx_output(int(self.index))
 			logging.debug(self.tag + ': sent the next index')
